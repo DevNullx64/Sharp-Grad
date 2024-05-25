caabde4aadcc25edd9e539fe8cc1c6ec4ba58d6e
Luc BOLOGNA <bologna.luc@gmail.com>
2024-05-25 23:26:14 +0200

    -

--- SharpGrad/Tensors/Tensor.cs
+++ SharpGrad/Tensors/Tensor.cs
@@ -1,8 +1,5 @@
-﻿using ILGPU;
-using ILGPU.Runtime;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Formats.Tar;
 using System.Linq;
 using System.Numerics;
@@ -13,26 +10,6 @@ using System.Threading.Tasks;
 
 namespace SharpGrad.Tensors
 {
-    public static class Tensors
-    {
-        private static Context GetContext()
-        {
-            Context result = Context.Create(builder => builder.AllAccelerators());
-            Debug.WriteLine($"Context created: {result}");
-            return result;
-        }
-        private static readonly Context context = GetContext();
-
-        private static Device GetDevice(Context context)
-        {
-            Device result = context.GetPreferredDevice(preferCPU: false);
-            Debug.WriteLine($"Device created: {result}");
-            return result;
-        }
-        private static readonly Device device = GetDevice(context);
-        public static readonly Accelerator Accelerator = device.CreateAccelerator(context);
-
-    }
 
     public interface ITensor {
         bool IsOnGpu { get; }
---
+++ SharpGrad/Tensors/Tensors.cs
@@ -0,0 +1,27 @@
+﻿using ILGPU;
+using ILGPU.Runtime;
+using System.Diagnostics;
+
+namespace SharpGrad.Tensors
+{
+    public static class Tensors
+    {
+        private static Context GetContext()
+        {
+            Context result = Context.Create(builder => builder.AllAccelerators());
+            Debug.WriteLine($"Context created: {result}");
+            return result;
+        }
+        private static readonly Context context = GetContext();
+
+        private static Device GetDevice(Context context)
+        {
+            Device result = context.GetPreferredDevice(preferCPU: false);
+            Debug.WriteLine($"Device created: {result}");
+            return result;
+        }
+        private static readonly Device device = GetDevice(context);
+        public static readonly Accelerator Accelerator = device.CreateAccelerator(context);
+
+    }
+}
