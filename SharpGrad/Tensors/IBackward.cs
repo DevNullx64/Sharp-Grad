9655a563d393a91cf64ff38be73a606381b229d7
Luc BOLOGNA <bologna.luc@gmail.com>
2024-05-28 17:21:51 +0200

    Merge IApplyOp and IBackward cs file

--- SharpGrad/Tensors/IBackward.cs
+++ SharpGrad/Tensors/IBackward.cs
@@ -3,6 +3,16 @@ using System.Numerics;
 
 namespace SharpGrad.Tensors
 {
+    public interface IBackward { }
+
+    public interface IBackwardOne<TType, TGrad> : IApplyOpOne<TType>
+        where TType : unmanaged, INumber<TType>
+        where TGrad : unmanaged, IFloatingPoint<TGrad>
+    {
+        abstract static TGrad BackwardCpu(TGrad grad, TType left);
+        abstract static void BackwardGpu(Index1D idx, ArrayView<TGrad> grad, ArrayView<TType> left, ArrayView<TGrad> leftGrad);
+    }
+
     public interface IBackwardTwo<TType, TGrad> : IApplyOpTwo<TType>
         where TType : unmanaged, INumber<TType>
         where TGrad : unmanaged, IFloatingPoint<TGrad>
@@ -10,4 +20,5 @@ namespace SharpGrad.Tensors
         abstract static (TGrad Left, TGrad Right) BackwardCpu(TGrad grad, TType left, TType right);
         abstract static void BackwardGpu(Index1D idx, ArrayView<TGrad> grad, ArrayView<TType> left, ArrayView<TType> right, ArrayView<TGrad> leftGrad, ArrayView<TGrad> rightGrad);
     }
+
 }
