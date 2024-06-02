cc7320d152e83f5218ef890d6720217e8d2d1694
Luc BOLOGNA <bologna.luc@gmail.com>
2024-06-02 15:30:36 +0200

    -

--- SharpGrad/Tensors/Operators/AddOp.cs
+++ SharpGrad/Tensors/Operators/AddOp.cs
@@ -9,10 +9,6 @@ namespace SharpGrad.Tensors
     {
         public static Shape ResultingShape(Shape operand1, Shape operand2) => operand1;
 
-        public AddOp()
-        {
-        }
-
         public static T Exec(T left, T right) => left + right;
         public static void Exec(Index1D idx, ArrayView1D<T, Stride1D.Dense> left, ArrayView1D<T, Stride1D.Dense> right, ArrayView1D<T, Stride1D.Dense> result)
             => result[idx] = Exec(left[idx], right[idx]);
