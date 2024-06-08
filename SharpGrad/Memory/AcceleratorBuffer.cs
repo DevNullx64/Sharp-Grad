56fd639b90c58f7a051525b464a2a42a698c1bb3
Luc BOLOGNA <bologna.luc@gmail.com>
2024-06-08 14:09:53 +0200

    Add some documentation

--- SharpGrad/Memory/AcceleratorBuffer.cs
+++ SharpGrad/Memory/AcceleratorBuffer.cs
@@ -14,15 +14,46 @@ using System.Threading;
 
 namespace SharpGrad.Memory
 {
+    /// <summary>
+    /// Internal interface for class that can create an <see cref="AcceleratorBuffer{T}"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of the data.</typeparam>
     internal interface ICreateAcceleratorBuffer<T>
         where T : unmanaged, INumber<T>, IPowerFunctions<T>
     {
+        /// <summary>
+        /// Create a new <see cref="AcceleratorBuffer{T}"/> with the specified length.
+        /// </summary>
+        /// <param name="length">The length of the data.</param>
+        /// <returns>A new <see cref="AcceleratorBuffer{T}"/> with the specified length.</returns>
         abstract static AcceleratorBuffer<T> Create(long length);
+
+        /// <summary>
+        /// Create a new <see cref="AcceleratorBuffer{T}"/> with the specified data.
+        /// </summary>
+        /// <param name="data">The data to be copied to the RAM.</param>
+        /// <returns>A new <see cref="AcceleratorBuffer{T}"/> with the specified data.</returns>
         abstract static AcceleratorBuffer<T> Create(T[] data);
+
+        /// <summary>
+        /// Create a new <see cref="AcceleratorBuffer{T}"/> with the specified data.
+        /// </summary>
+        /// <param name="data">The data to be copied to the RAM.</param>
+        /// <returns>A new <see cref="AcceleratorBuffer{T}"/> with the specified data.</returns>
         abstract static AcceleratorBuffer<T> Create(AcceleratorBuffer<T> data);
+
+        /// <summary>
+        /// Create a new <see cref="AcceleratorBuffer{T}"/> with the specified data.
+        /// </summary>
+        /// <param name="data">The data to be copied to the RAM.</param>
+        /// <returns>A new <see cref="AcceleratorBuffer{T}"/> with the specified data.</returns>
         abstract static AcceleratorBuffer<T> Create(MemoryBuffer1D<T, Stride1D.Dense> data);
     }
 
+    /// <summary>
+    /// A structure that manages data on the RAM and a <see cref="Accelerator"/> (GPU). It free the RAM data when the data is available on the <see cref="Accelerator"/>. And vice versa.
+    /// </summary>
+    /// <param name="length">The length of the data.</param>
     public abstract class AcceleratorBuffer(long length) : IAcceleratorBuffer
     {
         /// <summary>
@@ -48,6 +79,9 @@ namespace SharpGrad.Memory
         /// </summary>
         public bool IsEmpty { get => Location == BufferLocation.Empty; }
 
+        /// <summary>
+        /// Dispose the buffer.
+        /// </summary>
         protected virtual void Dispose(bool disposing)
         {
             if (!disposedValue)
@@ -63,6 +97,7 @@ namespace SharpGrad.Memory
             }
         }
 
+        /// <inheritdoc/>
         public void Dispose()
         {
             Dispose(disposing: true);
