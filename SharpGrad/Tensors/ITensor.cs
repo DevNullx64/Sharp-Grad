08ebc059007656e06d7ac398828dc776b090dc64
Luc BOLOGNA <bologna.luc@gmail.com>
2024-06-16 15:49:26 +0200

    Add documentation on ITensor and ITensor<T>

--- SharpGrad/Tensors/ITensor.cs
+++ SharpGrad/Tensors/ITensor.cs
@@ -3,15 +3,36 @@ using System;
 
 namespace SharpGrad.Tensors
 {
+    /// <summary>
+    /// Abstract tensor interface.
+    /// </summary>
     public interface ITensor : IEquatable<ITensor>
     {
+        /// <summary>
+        /// The shape of the tensor.
+        /// </summary>
         Shape Shape { get; set; }
+
+        /// <summary>
+        /// The length of the tensor.
+        /// </summary>
         long Length { get; }
+
+        /// <summary>
+        /// The computational depth of the tensor.
+        /// </summary>
+        long Depth { get; }
     }
 
+    /// <summary>
+    /// Interface for a tensor with generic type.
+    /// </summary>
     public interface ITensor<T> : ITensor
     where T : unmanaged, INumber<T>, IPowerFunctions<T>
     {
+        /// <summary>
+        /// Access the tensor element at the specified indices.
+        /// </summary>
         T this[params Index[] indices] { get; }
     }
 }
