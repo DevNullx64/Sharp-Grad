caf93e8aa0b7a4d26af0259e0aa6538202f09f35
Luc BOLOGNA <bologna.luc@gmail.com>
2024-07-27 14:50:05 +0200

    Creat how file for IBufferManager and ILowLevelMemoryManager

---
+++ SharpGrad/Tensors/IBufferManager.cs
@@ -0,0 +1,50 @@
+﻿using SharpGrad.Memory;
+using System;
+
+namespace SharpGrad.Tensors
+{
+    /// <summary>
+    /// Interface for memory management.
+    /// </summary>
+    public interface IBufferManager
+    {
+        /// <summary>
+        /// Gets an <see cref="AcceleratorBuffer"/> with the specified length.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements.</typeparam>
+        /// <param name="length">The length of the buffer.</param>
+        /// <returns>The allocated buffer.</returns>
+        /// <exception cref="OutOfMemoryException">Failed to allocate memory for the buffer.</exception>
+        AcceleratorBuffer<T> GetBuffer<T>(long length)
+            where T : unmanaged;
+
+        /// <summary>
+        /// Gets an <see cref="AcceleratorBuffer"/> from the given values.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements.</typeparam>
+        /// <param name="values">The values to copy to the buffer.</param>
+        /// <returns>The allocated buffer.</returns>
+        /// <exception cref="OutOfMemoryException">Failed to allocate memory for the buffer.</exception>
+        AcceleratorBuffer<T> GetBuffer<T>(T[] values)
+            where T : unmanaged;
+
+        /// <summary>
+        /// Releases the given buffer.
+        /// </summary>
+        void Release(AcceleratorBuffer buffer);
+
+        /// <summary>
+        /// Offloads memory from the accelerator to the RAM.
+        /// </summary>
+        /// <param name="length">The length of memory to offload.</param>
+        /// <returns>The amount of memory offloaded.</returns>
+        /// <remarks>Offloads the least recently used memory first.</remarks>
+        long OffloadMemory(long length = 0);
+
+        /// <summary>
+        /// Synchronizes to pending operations.
+        /// </summary>
+        /// <remarks>Ensures that all operations are completed.</remarks>
+        void Synchronize();
+    }
+}
---
+++ SharpGrad/Tensors/ILowLevelMemoryManager.cs
@@ -0,0 +1,52 @@
+﻿using ILGPU;
+using ILGPU.Runtime;
+using SharpGrad.Memory;
+using System;
+
+namespace SharpGrad.Tensors
+{
+    /// <summary>
+    /// Interface for low-level memory management.
+    /// </summary>
+    public interface ILowLevelMemoryManager
+    {
+        /// <summary>
+        /// Allocates a 1D memory buffer.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements.</typeparam>
+        /// <param name="length">The length of the buffer.</param>
+        /// <returns>The allocated memory buffer.</returns>
+        /// <exception cref="OutOfMemoryException">Failed to allocate memory for the buffer.</exception>
+        MemoryBuffer1D<T, Stride1D.Dense> MemoryBuffer1D<T>(long length)
+            where T : unmanaged;
+
+        /// <summary>
+        /// Allocates a 1D memory buffer from the given values.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements.</typeparam>
+        /// <param name="values">The values to copy to the buffer.</param>
+        /// <returns>The allocated memory buffer.</returns>
+        /// <exception cref="OutOfMemoryException">Failed to allocate memory for the buffer.</exception>
+        MemoryBuffer1D<T, Stride1D.Dense> MemoryBuffer1D<T>(T[] values)
+            where T : unmanaged;
+
+        /// <summary>
+        /// Allocates a 1D memory buffer from the given data.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements.</typeparam>
+        /// <param name="data">The data to copy to the buffer.</param>
+        /// <returns>The allocated memory buffer.</returns>
+        /// <exception cref="OutOfMemoryException">Failed to allocate memory for the buffer.</exception>
+        AcceleratorBuffer<T> GetBuffer<T>(MemoryBuffer1D<T, Stride1D.Dense> data)
+            where T : unmanaged;
+
+        /// <summary>
+        /// Fills the given accelerator data with the specified value.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements.</typeparam>
+        /// <param name="acceleratorData">The accelerator data to fill.</param>
+        /// <param name="value">The value to fill the data with.</param>
+        void Fill<T>(MemoryBuffer1D<T, Stride1D.Dense> acceleratorData, T value)
+            where T : unmanaged;
+    }
+}
