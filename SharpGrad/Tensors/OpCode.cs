a4149d3c05f7517a9dd0888712b133e6b724ca0f
Luc BOLOGNA <bologna.luc@gmail.com>
2024-07-27 15:03:36 +0200

    Externalise some class from OperationKPU.cs file

---
+++ SharpGrad/Tensors/OpCode.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace SharpGrad.Tensors
+{
+    /// <summary>
+    /// An operation to perform using the KPU
+    /// </summary>
+    public enum OpCode : short
+    {
+
+        #region Flags
+        /// <summary>
+        /// If set, the operation is commutative. Otherwise, it is not.
+        /// </summary>
+        Commutative = 0x200,
+
+        /// <summary>
+        /// If set, the operation is unary. Otherwise, it is binary.
+        /// </summary>
+        Unary = 0x400,
+
+        /// <summary>
+        /// If set, the operation is a reduction. Otherwise, it is not.
+        /// </summary>
+        Reduction = 0x800,
+        #endregion
+
+        #region Utilities
+        None = -1,
+
+        [Obsolete("!!! Not implemented yet !!!")]
+        Reset = 0,
+
+        /// <summary>
+        /// Store the result in the left operand
+        /// </summary>
+        Store = 1 | Unary,
+        #endregion
+
+        #region Arithmetic
+        /// <summary>
+        /// Add the left operand to the right operand
+        /// </summary>
+        Add = 5 | Commutative,
+
+        /// <summary>
+        /// Subtract the right operand from the left operand
+        /// </summary>
+        Sub = 6,
+
+        /// <summary>
+        /// Negate the left operand
+        /// </summary>
+        Neg = Sub | Unary,
+
+        /// <summary>
+        /// Multiply the left operand by the right operand
+        /// </summary>
+        Mul = 7 | Commutative,
+
+        /// <summary>
+        /// Divide the left operand by the right operand
+        /// </summary>
+        Div = 8,
+        #endregion
+
+        #region Trigonometry !!! Not implemented yet !!!
+        [Obsolete("!!! Not implemented yet !!!")]
+        Pow = 16,
+        [Obsolete("!!! Not implemented yet !!!")]
+        Log = 17 | Unary,
+        [Obsolete("!!! Not implemented yet !!!")]
+        Exp = 18 | Unary,
+
+        [Obsolete("!!! Not implemented yet !!!")]
+        Abs = 19 | Unary,
+        [Obsolete("!!! Not implemented yet !!!")]
+        Sqrt = 20 | Unary,
+        [Obsolete("!!! Not implemented yet !!!")]
+        Sin = 21 | Unary,
+        [Obsolete("!!! Not implemented yet !!!")]
+        Cos = 22 | Unary,
+        [Obsolete("!!! Not implemented yet !!!")]
+        Tan = 23 | Unary,
+        #endregion
+
+        #region Reductions !!! Not implemented yet !!!
+        [Obsolete("!!! Not implemented yet !!!")]
+        Sum = 32 | Reduction,
+        [Obsolete("!!! Not implemented yet !!!")]
+        Prod = 33 | Reduction,
+        [Obsolete("!!! Not implemented yet !!!")]
+        Min = 34 | Reduction,
+        [Obsolete("!!! Not implemented yet !!!")]
+        Max = 35 | Reduction,
+        [Obsolete("!!! Not implemented yet !!!")]
+        Mean = 36 | Reduction,
+        [Obsolete("!!! Not implemented yet !!!")]
+        Var = 37 | Reduction,
+        [Obsolete("!!! Not implemented yet !!!")]
+        Std = 38 | Reduction,
+        #endregion
+    }
+}
--- SharpGrad/Tensors/OperationKPU.cs
+++ SharpGrad/Tensors/OperationKPU.cs
@@ -1,118 +1,10 @@
-﻿using ILGPU;
-using ILGPU.Runtime;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data;
-using System.Numerics;
 using System.Reflection.Emit;
 using System.Runtime.InteropServices.JavaScript;
 
 namespace SharpGrad.Tensors
 {
-    public enum NativeType : int
-    {
-        Unknown = 0,
-        Bits8 = 1, Bits16 = 2, Bits32 = 3, Bits64 = 4,
-        Signed = 8, Unsigned = 16, FloatingPoint = 24,
-
-        Double = Bits64 | FloatingPoint,
-        Float = Bits32 | FloatingPoint,
-        Long = Bits64 | Signed,
-        ULong = Bits64 | Unsigned,
-        Int = Bits32 | Signed,
-        UInt = Bits32 | Unsigned,
-        Short = Bits16 | Signed,
-        UShort = Bits16 | Unsigned,
-        Byte = Bits16 | Unsigned,
-        SByte = Bits16 | Signed,
-    }
-
-    /// <summary>
-    /// An operation to perform using the KPU
-    /// </summary>
-    public enum OpCode : short
-    {
-        None = -1,
-
-        /// <summary>
-        /// If set, the operation is commutative. Otherwise, it is not.
-        /// </summary>
-        Commutative = 0x100,
-
-        /// <summary>
-        /// If set, the operation is unary. Otherwise, it is binary.
-        /// </summary>
-        Unary = 0x200,
-
-        /// <summary>
-        /// If set, the operation is a reduction. Otherwise, it is not.
-        /// </summary>
-        Reduction = 0x400,
-
-        [Obsolete("/!\\ Not implemented yet /!\\")]
-        Reset = 0,
-        Store = 1 | Unary,
-        Add = 5 | Commutative,
-        Sub = 6,
-        Neg = Sub | Unary,
-        Mul = 7 | Commutative,
-        Div = 8,
-        [Obsolete("/!\\ Not implemented yet /!\\")]
-        Pow = 16,
-        [Obsolete("/!\\ Not implemented yet /!\\")]
-        Log = 17 | Unary,
-        [Obsolete("/!\\ Not implemented yet /!\\")]
-        Exp = 18 | Unary,
-
-        [Obsolete("/!\\ Not implemented yet /!\\")]
-        Abs = 19 | Unary,
-        [Obsolete("/!\\ Not implemented yet /!\\")]
-        Sqrt = 20 | Unary,
-        [Obsolete("/!\\ Not implemented yet /!\\")]
-        Sin = 21 | Unary,
-        [Obsolete("/!\\ Not implemented yet /!\\")]
-        Cos = 22 | Unary,
-        [Obsolete("/!\\ Not implemented yet /!\\")]
-        Tan = 23 | Unary,
-
-        [Obsolete("/!\\ Not implemented yet /!\\")]
-        Sum = 32 | Reduction,
-        [Obsolete("/!\\ Not implemented yet /!\\")]
-        Prod = 33 | Reduction,
-        Min = 34 | Reduction,
-        [Obsolete("/!\\ Not implemented yet /!\\")]
-        Max = 35 | Reduction,
-        [Obsolete("/!\\ Not implemented yet /!\\")]
-        Mean = 36 | Reduction,
-        [Obsolete("/!\\ Not implemented yet /!\\")]
-        Var = 37 | Reduction,
-        [Obsolete("/!\\ Not implemented yet /!\\")]
-        Std = 38 | Reduction,
-
-    }
-
-    public interface IBufferOperande
-    {
-        long Length { get; }
-    }
-
-    public readonly struct MemBufferOperande<T>(MemoryBuffer1D<T, Stride1D.Dense> buffer) : IBufferOperande
-        where T : unmanaged, INumber<T>
-    {
-        public long Length => buffer.Length;
-        public MemoryBuffer1D<T, Stride1D.Dense> Buffer => buffer;
-    }
-
-    public interface IBufferOperation : IBufferOperande
-    {
-        /// <summary>
-        /// Operation to perform
-        /// </summary>
-        OpCode OpCode { get; }
-        IBufferOperande Buffer1 { get; }
-        IBufferOperande Buffer2 { get; }
-    }
-
     /// <summary>
     /// An operation to perform using the KPU
     /// </summary>
@@ -124,7 +16,7 @@ namespace SharpGrad.Tensors
         /// <summary>
         /// Value used to represent an empty index
         /// </summary>
-        public const short NoOperand = short.MaxValue;
+        public const short NoOperand = short.MinValue;
 
         /// <inheritdoc/>
         public OpCode OpCode => opCode;
