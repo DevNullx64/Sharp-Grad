cce3418220228493beb8026337ee6fc49f556038
Luc BOLOGNA <bologna.luc@gmail.com>
2024-07-27 15:04:34 +0200

    Add extend method on type List<T?> to insert element at the first null place or add it

--- SharpGrad/Tensors/KPU/KpuBackwardScript.cs
+++ SharpGrad/Tensors/KPU/KpuBackwardScript.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ILGPU.Runtime.Cuda;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
@@ -8,38 +9,60 @@ namespace SharpGrad.Tensors
     public class KpuBackwardScript<T> : KpuScrip<T>
         where T : unmanaged, INumber<T>, IPowerFunctions<T>, IExponentialFunctions<T>, ILogarithmicFunctions<T>
     {
+        private enum TreatmentState: byte
+        {
+            NotTreated = 0,
+            LeftBranchTreated = 1,
+            RightBranchTreated = 2,
+            Treated = 3
+        }
+        protected readonly List<TensorData<T>> gradients = [];
+
+        /// <summary>
+        /// List of gradients to compute.
+        /// </summary>
+        public IReadOnlyList<TensorData<T>> Gradients => gradients;
+
         public KpuBackwardScript(Tensor<T> tensor)
         {
-            var topo = tensor.DepthFirstSearch()
+            // Topological sort of the graph that needs gradient computation
+            List<Tensor<T>> topo = tensor.DepthFirstSearch(true)
                 .OrderBy(e => e.Value.Index)
                 .Select(e => e.Value.Tensor)
                 .ToList();
 
-            // 0: not treated
-            // 1: only deep branch treated
-            // 2: shallow branch also treated / treatment is finished
-            byte[] treated = new byte[topo.Count];
+            // List of data tensors that needs gradient computation
+            gradients = topo.OfType<TensorData<T>>().ToList();
 
-            List<ITensor<T>> dataList = [];
-            List<ITensor<T>?> registerList = [];
-            Stack<T> Gradients = new();
-            Gradients.Push(T.CreateTruncating(1));
+            TreatmentState[] treated = new TreatmentState[topo.Count];
+            T[] gradients_ = new T[topo.Count];
+
+            List<Tensor<T>?> cacheList = [];
+            Stack<T> globalGradients = new();
+            globalGradients.Push(T.One);
 
             // Follow the deep branch
-            Tensor<T> current = topo[topo.Count - 1];
+            Tensor<T> current = topo[^1];
             while (current.OperandCount > 0)
             {
                 if (current.OperandCount == 1)
                 {
                     ITensorOperation1<T> operation1 = (ITensorOperation1<T>)current;
-                    treated[topo.IndexOf(operation1.Operand)] = 2;
+                    treated[topo.IndexOf(operation1.Operand)] = TreatmentState.Treated;
                     current = operation1.Operand;
                 }
                 else
                 {
                     ITensorOperation2<T> operation2 = (ITensorOperation2<T>)current;
-                    treated[topo.IndexOf(operation2.Operand1)] = 1;
-                    current = operation2.Operand1.Depth >= operation2.Operand2.Depth ? operation2.Operand1 : operation2.Operand2;
+                    if(operation2.Operand1.Depth >= operation2.Operand2.Depth)
+                    {
+                        current = operation2.Operand1;
+                        treated[topo.IndexOf(operation2.Operand2)] = TreatmentState.LeftBranchTreated;
+                    } else
+                    {
+                        current = operation2.Operand2;
+                        treated[topo.IndexOf(operation2.Operand1)] = TreatmentState.RightBranchTreated;
+                    }
                 }
             }
 
@@ -48,12 +71,6 @@ namespace SharpGrad.Tensors
             {
                 var t = topo[i];
 
-                if (!t.NeedsGradient)
-                {
-                    treated[i] = 2;
-                    continue;
-                }
-
                 OpCode opCode;
                 short iOp1;
                 short iOp2;
@@ -61,8 +78,8 @@ namespace SharpGrad.Tensors
                 if (t.Depth == 0)
                 {
                     // Add the data tensor to the list of data tensors only if it is not already present
-                    if (!dataList.Contains(t))
-                        dataList.Add(t);
+                    if (!datas.Contains(t))
+                        datas.Add(t);
 
 
                     int uCount = UsageCount(t, topo, i);
@@ -71,12 +88,12 @@ namespace SharpGrad.Tensors
                         continue;
 
                     opCode = OpCode.Store;
-                    iOp1 = (short)dataList.IndexOf(t);
+                    iOp1 = (short)datas.IndexOf(t);
                     iOp2 = OperationKPU.NoOperand;
                 }
                 else
                 {
-                    if (registerList.Contains(t))
+                    if (cacheList.Contains(t))
                         continue;
 
                     if (t.OperandCount == 1 && t is ITensorOperation1<T> operation1)
@@ -84,11 +101,11 @@ namespace SharpGrad.Tensors
                         opCode = operation1.OpCode;
 
                         // Operation result or stored data should contains the operand
-                        iOp1 = (short)registerList.IndexOf(operation1.Operand);
+                        iOp1 = (short)cacheList.IndexOf(operation1.Operand);
                         if (iOp1 == -1)
                         {
                             // If not, it's a data used only once
-                            iOp1 = (short)dataList.IndexOf(operation1.Operand);
+                            iOp1 = (short)datas.IndexOf(operation1.Operand);
                             // If not, something is wrong !
                             if (iOp1 == -1)
                                 throw new Exception($"Index {i} ({operation1}) : Operand 1 {operation1.Operand} not found.");
@@ -97,7 +114,7 @@ namespace SharpGrad.Tensors
                         {
                             // If the operand is not used anymore, free the register
                             if (!WillBeUsed(operation1.Operand, topo, i))
-                                registerList[iOp1] = null;
+                                cacheList[iOp1] = null;
                             // Compute the KPU register index
                             iOp1 = (short)(-iOp1 - 1);
                         }
@@ -108,11 +125,11 @@ namespace SharpGrad.Tensors
                         opCode = operation2.OpCode;
 
                         // Operation result or stored data should contains the first operand
-                        iOp1 = (short)registerList.IndexOf(operation2.Operand1);
+                        iOp1 = (short)cacheList.IndexOf(operation2.Operand1);
                         if (iOp1 == -1)
                         {
                             // If not, it's a data used only once
-                            iOp1 = (short)dataList.IndexOf(operation2.Operand1);
+                            iOp1 = (short)datas.IndexOf(operation2.Operand1);
                             // If not, something is wrong !
                             if (iOp1 == -1)
                                 throw new Exception($"Index {i} ({operation2}) : Operand 1 {operation2.Operand1} not found.");
@@ -121,17 +138,17 @@ namespace SharpGrad.Tensors
                         {
                             // If the operand is not used anymore, free the register
                             if (!WillBeUsed(operation2.Operand1, topo, i))
-                                registerList[iOp1] = null;
+                                cacheList[iOp1] = null;
                             // Compute the KPU register index
                             iOp1 = (short)(-iOp1 - 1);
                         }
 
                         // Operation result or stored data should contains the second operand
-                        iOp2 = (short)registerList.IndexOf(operation2.Operand2);
+                        iOp2 = (short)cacheList.IndexOf(operation2.Operand2);
                         if (iOp2 == -1)
                         {
                             // If not, it's a data used only once
-                            iOp2 = (short)dataList.IndexOf(operation2.Operand2);
+                            iOp2 = (short)datas.IndexOf(operation2.Operand2);
                             // If not, something is wrong !
                             if (iOp2 == -1)
                                 throw new Exception($"Index {i} ({operation2}) : Operand 2 {operation2.Operand2} not found.");
@@ -140,7 +157,7 @@ namespace SharpGrad.Tensors
                         {
                             // If the operand is not used anymore, free the register
                             if (!WillBeUsed(operation2.Operand2, topo, i))
-                                registerList[iOp2] = null;
+                                cacheList[iOp2] = null;
                             // Compute the KPU register index
                             iOp2 = (short)(-iOp2 - 1);
                         }
@@ -150,12 +167,11 @@ namespace SharpGrad.Tensors
                 }
 
                 // If the operation is not the last one, the result is stored in a register. Otherwise, it's the final result.
-                short iResult = (short)(i != topo.Count - 1 ? -Store(registerList, t) - 1 : OperationKPU.NoOperand);
+                short iResult = (short)(i != topo.Count - 1 ? -Store(cacheList, t) - 1 : OperationKPU.NoOperand);
                 operations.Add(new OperationKPU(opCode, iResult, iOp1, iOp2));
             }
 
-            datas.AddRange(dataList.Cast<TensorData<T>>());
-            CacheSize = (byte)registerList.Count;
+            CacheSize = (byte)cacheList.Count;
         }
     }
 }
---
+++ SharpGrad/Tensors/KPU/ListExtender.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpGrad.Tensors.KPU
+{
+    public static class ListExtender
+    {
+        /// <summary>
+        /// Inserts an item into the list at the first available null index. Add the item to the end of the list if no null index is found.
+        /// </summary>
+        /// <typeparam name="T">The type of the list.</typeparam>
+        /// <param name="this">The list to insert the item into.</param>
+        /// <param name="item">The item to insert.</param>
+        /// <returns>The index of the inserted item.</returns>
+        public static int Insert<T>(this List<T?> @this, T item)
+        {
+            int count = @this.Count;
+            for (int i = 0; i < count; i++)
+                if (@this[i] is null)
+                {
+                    @this[i] = item;
+                    return i;
+                }
+            @this.Add(item);
+            return count;
+        }
+    }
+}
