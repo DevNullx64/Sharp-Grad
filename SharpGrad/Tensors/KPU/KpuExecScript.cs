067d6e243794bab90773917df5afa9ef723736d1
Luc BOLOGNA <bologna.luc@gmail.com>
2024-07-27 14:51:17 +0200

    Use Tensor<T> instead of ITensor<T>

--- SharpGrad/Tensors/KPU/KpuExecScript.cs
+++ SharpGrad/Tensors/KPU/KpuExecScript.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SharpGrad.Tensors.KPU;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
@@ -15,8 +16,7 @@ namespace SharpGrad.Tensors
                 .Select(e => e.Value.Tensor)
                 .ToList();
 
-            List<ITensor<T>> dataList = [];
-            List<ITensor<T>?> registerList = [];
+            List<Tensor<T>?> cacheList = [];
 
             for (int i = 0; i < topo.Count; i++)
             {
@@ -28,8 +28,8 @@ namespace SharpGrad.Tensors
                 if (t.Depth == 0)
                 {
                     // Add the data tensor to the list of data tensors only if it is not already present
-                    if (!dataList.Contains(t))
-                        dataList.Add(t);
+                    if (!datas.Contains(t))
+                        datas.Add(t);
 
 
                     int uCount = UsageCount(t, topo, i);
@@ -38,12 +38,12 @@ namespace SharpGrad.Tensors
                         continue;
 
                     opCode = OpCode.Store;
-                    iOp1 = (short)dataList.IndexOf(t);
+                    iOp1 = (short)datas.IndexOf(t);
                     iOp2 = OperationKPU.NoOperand;
                 }
                 else
                 {
-                    if (registerList.Contains(t))
+                    if (cacheList.Contains(t))
                         continue;
 
                     if (t.OperandCount == 1 && t is ITensorOperation1<T> operation1)
@@ -51,11 +51,11 @@ namespace SharpGrad.Tensors
                         opCode = operation1.OpCode;
 
                         // Operation result or stored data should contains the operand
-                        iOp1 = (short)registerList.IndexOf(operation1.Operand);
+                        iOp1 = (short)cacheList.IndexOf(operation1.Operand);
                         if (iOp1 == -1)
                         {
                             // If not, it's a data used only once
-                            iOp1 = (short)dataList.IndexOf(operation1.Operand);
+                            iOp1 = (short)datas.IndexOf(operation1.Operand);
                             // If not, something is wrong !
                             if (iOp1 == -1)
                                 throw new Exception($"Index {i} ({operation1}) : Operand 1 {operation1.Operand} not found.");
@@ -64,7 +64,7 @@ namespace SharpGrad.Tensors
                         {
                             // If the operand is not used anymore, free the register
                             if (!WillBeUsed(operation1.Operand, topo, i))
-                                registerList[iOp1] = null;
+                                cacheList[iOp1] = null;
                             // Compute the KPU register index
                             iOp1 = (short)(-iOp1 - 1);
                         }
@@ -75,11 +75,11 @@ namespace SharpGrad.Tensors
                         opCode = operation2.OpCode;
 
                         // Operation result or stored data should contains the first operand
-                        iOp1 = (short)registerList.IndexOf(operation2.Operand1);
+                        iOp1 = (short)cacheList.IndexOf(operation2.Operand1);
                         if (iOp1 == -1)
                         {
                             // If not, it's a data used only once
-                            iOp1 = (short)dataList.IndexOf(operation2.Operand1);
+                            iOp1 = (short)datas.IndexOf(operation2.Operand1);
                             // If not, something is wrong !
                             if (iOp1 == -1)
                                 throw new Exception($"Index {i} ({operation2}) : Operand 1 {operation2.Operand1} not found.");
@@ -88,17 +88,17 @@ namespace SharpGrad.Tensors
                         {
                             // If the operand is not used anymore, free the register
                             if (!WillBeUsed(operation2.Operand1, topo, i))
-                                registerList[iOp1] = null;
+                                cacheList[iOp1] = null;
                             // Compute the KPU register index
                             iOp1 = (short)(-iOp1 - 1);
                         }
 
                         // Operation result or stored data should contains the second operand
-                        iOp2 = (short)registerList.IndexOf(operation2.Operand2);
+                        iOp2 = (short)cacheList.IndexOf(operation2.Operand2);
                         if (iOp2 == -1)
                         {
                             // If not, it's a data used only once
-                            iOp2 = (short)dataList.IndexOf(operation2.Operand2);
+                            iOp2 = (short)datas.IndexOf(operation2.Operand2);
                             // If not, something is wrong !
                             if (iOp2 == -1)
                                 throw new Exception($"Index {i} ({operation2}) : Operand 2 {operation2.Operand2} not found.");
@@ -107,7 +107,7 @@ namespace SharpGrad.Tensors
                         {
                             // If the operand is not used anymore, free the register
                             if (!WillBeUsed(operation2.Operand2, topo, i))
-                                registerList[iOp2] = null;
+                                cacheList[iOp2] = null;
                             // Compute the KPU register index
                             iOp2 = (short)(-iOp2 - 1);
                         }
@@ -117,12 +117,12 @@ namespace SharpGrad.Tensors
                 }
 
                 // If the operation is not the last one, the result is stored in a register. Otherwise, it's the final result.
-                short iResult = (short)(i != topo.Count - 1 ? -Store(registerList, t) - 1 : OperationKPU.NoOperand);
+                short iResult;
+                checked { iResult = (short)(i != topo.Count - 1 ? ~cacheList.Insert(t) : OperationKPU.NoOperand); }
                 operations.Add(new OperationKPU(opCode, iResult, iOp1, iOp2));
             }
 
-            datas.AddRange(dataList.Cast<TensorData<T>>());
-            CacheSize = (byte)registerList.Count;
+            CacheSize = (byte)cacheList.Count;
         }
     }
 }
